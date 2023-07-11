38425949c924d347174df020303b76e17bd07217
Niki Nezakati <59639193+nikinezakati@users.noreply.github.com>
2023-07-11 18:12:26 +0330

    Delete src directory

--- src/constants.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DATA_URL =  "data" 
-STATS_DATA_DIR = "stats"
-RAW_DATA_DIR = os.path.join(BASE_DATA_URL, 'raw') 
-PREPROCCESSED_DATA_DIR = os.path.join(BASE_DATA_URL, 'preprocessed') 
-BROKEN_WORD_DATA_DIR = os.path.join(BASE_DATA_URL, 'wordbroken') 
-BROKEN_SENT_DATA_DIR = os.path.join(BASE_DATA_URL, 'sentencebroken') 
-FULL_SENTENCE_DATA_DIR = os.path.join(BASE_DATA_URL, 'full_sentence_data') 
